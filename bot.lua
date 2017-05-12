1e110b0c5f78699bceb377868295125e7bd1bf9f
3pehrdev <sepehrsalehi0111@gmail.com>
2017-05-12 15:22:00 +0430

    Update bot.lua

--- bot.lua
+++ bot.lua
@@ -189,6 +189,46 @@ function msg_processor(msg)
 			file:close()
 			sendMessage(msg.chat.id, "Done !")
 		end
+			----------
+			if msg.text:match("^c(.*)$") then
+		local receiver = get_receiver(msg)
+     name = get_name(msg)
+     text = ''
+  if is_admin(msg) then
+        bash = msg.text:sub(4,-1)
+        text = run_bash(bash)
+     else
+        text = name .. ' you have no power here!'
+     end
+     send_large_msg(receiver, 'In Process...', ok_cb, false)
+     return text
+end
+
+function run_bash(str)
+    local cmd = io.popen(str)
+    local result = cmd:read('*all')
+    cmd:close()
+    return result
+end
+
+function on_getting_dialogs(cb_extra,success,result)
+  if success then
+    local dialogs={}
+    for key,value in pairs(result) do 
+      for chatkey, chat in pairs(value.peer) do
+        print(chatkey,chat)
+        if chatkey=="id" then
+          table.insert(dialogs,chat.."\n")
+        end
+        if chatkey=="print_name" then
+          table.insert(dialogs,chat..": ")
+        end
+      end 
+    end
+--------
+    send_msg(cb_extra[1],table.concat(dialogs),ok_cb,false)
+  end
+end
 		if msg.text:match("^/shell (.*)$") then
 			local matches = { string.match(msg.text, "^/shell (.*)$") }
 			local text = io.popen('cd "'..BASE_FOLDER..currect_folder..'" && '..matches[1]:gsub('—', '--')):read('*all')
